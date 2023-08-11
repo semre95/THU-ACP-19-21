521d6badb98f4409d6cd3ea5b2dcbd0363766099
semre95 <36171298+semre95@users.noreply.github.com>
2023-08-11 15:56:55 +0300

    Add files via upload

---
+++ Computer Graphics/assignment2/Graphics/cmake-build-debug/CMakeFiles/SOIL.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SOIL.dir/src/SOIL/SOIL.c.o"
+  "CMakeFiles/SOIL.dir/src/SOIL/image_DXT.c.o"
+  "CMakeFiles/SOIL.dir/src/SOIL/image_helper.c.o"
+  "CMakeFiles/SOIL.dir/src/SOIL/stb_image_aug.c.o"
+  "libSOIL.a"
+  "libSOIL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SOIL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
